3c298c98e3055de1ad59cef7a326659e2026f880
Nikolay Khilyuk <nkh@ua.fm>
2020-07-08 21:48:43 +0300

    Add logger
    Signed-off-by: Nikolay Khilyuk <nkh@ua.fm>

--- eigen-hello-world/app/src/main/cpp/MainActivityJNI.cpp
+++ eigen-hello-world/app/src/main/cpp/MainActivityJNI.cpp
@@ -1,9 +1,15 @@
 #include <jni.h>
 #include <string>
 
+#define LOG_TAG "MainActivityJNI"
+#include "nkh-lab/logger.hpp"
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_eigenhelloworld_MainActivity_stringFromJNI(JNIEnv* env, jobject /* this */)
 {
+    LOG_FNC;
+
     std::string hello = "Hello from C++";
+    LOG_INF << hello;
     return env->NewStringUTF(hello.c_str());
 }
